af7cb0df0895bcfb33fdb5b8b1df476564d427a2
Michael Neuroth <michael.neuroth@freenet.de>
2023-11-03 19:56:20 +0100

    Improve unit tests

--- HaxeLisp/tests/LispTokenTest.hx
+++ HaxeLisp/tests/LispTokenTest.hx
@@ -5,5 +5,17 @@ class LispTokenTest extends utest.Test {
     public function testToken1() {
         var token = new LispToken.LispToken("1.2", 0, 2, 7);
         Assert.equals(1.2, token.Value);
+        Assert.equals(Type.enumConstructor(token.Type), "Double");
+        Assert.equals(0, token.StartPos);
+    }
+    public function testToken2() {
+        var token1 = new LispToken.LispToken("1.2", 0, 2, 7);
+        var token2 = new LispToken.LispToken("3.4", 0, 2, 7);
+        Assert.equals(token1.Type, token2.Type);
+    }
+    public function testToken3() {
+        var token1 = new LispToken.LispToken("1.2", 0, 2, 7);
+        var token2 = new LispToken.LispToken("3", 0, 2, 7);
+        //Assert.notEquals(token1.Type, token2.Type);
     }
 }
